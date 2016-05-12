954bb38842a2232391323e5b39cd9b93226b3e8e
Fernando Sanchez <fernandosanchezmunoz@gmail.com>
2016-05-11 20:11:11 -0400

    use  if defined 2

--- utils/launch.sh
+++ utils/launch.sh
@@ -24,7 +24,8 @@ usage() {
 NAME="$(basename $0)"
 REAL_NAME="$(readlink -f $0)"
 HERE="$(cd "$(dirname "$REAL_NAME")" && pwd)"
-PORT=$([[ $PORT0 != "" ]] && $PORT0 || "6080") #use $PORT0 if defined
+#PORT=$([ -z $PORT0 ] && "6080" || $PORT0)
+PORT=${PORT0:-"6080"} #use $PORT0 if defined
 VNC_DEST="localhost:5900"
 CERT=""
 WEB=""
